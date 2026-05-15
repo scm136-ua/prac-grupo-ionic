9e79e0da7028cf6256768d9d4457c41927ae1571
Fabián Godoy Silvestre <fgs36@alu.ua.es>
2026-05-15 13:36:13 +0200

    Arreglo toast para que salga al logearte y que exit te lleve al login

--- appIonic/src/app/app.component.ts
+++ appIonic/src/app/app.component.ts
@@ -1,34 +1,8 @@
 import { Component } from '@angular/core';
-import { ToastController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(
-    private toastController: ToastController,
-    private platform: Platform
-  ) {}
-
-
-  async ngOnInit() {
-    await this.platform.ready();
-
-    await this.showWelcomeToast();
-  }
-
-  async showWelcomeToast() {
-
-    const toast = await this.toastController.create({
-      message: 'Welcome to the Star Wars Wiki App!', 
-      duration: 3000, 
-      position: 'middle', 
-      color: 'primary', 
-      
-    });
-    
-    await toast.present();
-  }
-}
+export class AppComponent {}
