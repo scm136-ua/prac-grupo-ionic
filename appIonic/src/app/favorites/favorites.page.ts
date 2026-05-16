8b6d86decab5130dba7d9a0a115d4f3312eb1491
jsv39 <jsv39@alu.ua.es>
2026-05-16 11:09:11 +0200

    Pagina de favoritos y taller 4 acabados

--- appIonic/src/app/favorites/favorites.page.ts
+++ appIonic/src/app/favorites/favorites.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StorageService } from '../services/storage.service';
 import { MenuController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-favorites',
@@ -8,7 +9,20 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['favorites.page.scss']
 })
 export class FavoritesPage {
-  constructor(private menuController: MenuController) {}
+  constructor(private menuController: MenuController, private storageSrv: StorageService) {}
+
+  public favorites: any[] = [];
+
+  ngOnInit() {
+    this.storageSrv.get('favorites').then(data => {
+      this.favorites = data??[];
+    });
+  }
+
+  generateURL(cat: string, id: string) {
+    return "/tabs/wiki/article/" + cat + "/" + id;
+  }
+
 
   ionViewWillEnter() {
       this.menuController.enable(true, 'principal');
